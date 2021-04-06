cfdf4c13ecdbc7126b57a0dfe144182d3f921f2b
larry adams <ladams1776@yahoo.com>
2021-04-05 20:33:21 -0400

    add more functions

---  Bank.sol
+++  Bank.sol
@@ -10,8 +10,22 @@ contract Bank {
         addr: 0x66ec652455a86,
         amount:50
     });
+    
+    Account public anotherAccount = Account({
+        addr: 0x6686867f389357,
+        amount: 50
+    });
 
     function addAmount(uint _addMoney) public {
         acc.amount += _addMoney;
+    }
+    
+    function withdraw(uint _removeMoney) public {
+        acc.amount -= _removeMoney;
+    }
+    
+    function deductToSecondAccount(uint _deductMoney) public {
+        acc.amount -= _deductMoney;
+        anotherAccount.amount += _deductMoney;
     }
 }
